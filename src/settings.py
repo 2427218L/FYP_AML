09d340c0277b60ae49365b82adcd762ad77646da
2427218L <55675685+2427218L@users.noreply.github.com>
2020-04-01 15:13:47 +0800

    Update File Path

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,3 @@
-model_path = "./VGG_FACE.caffemodel"
-model_definition = './VGG_FACE_deploy.prototxt'
+model_path = "./vgg_face_caffe/VGG_FACE.caffemodel"
+model_definition = './vgg_face_caffe/VGG_FACE_deploy.prototxt'
 gpu = False
